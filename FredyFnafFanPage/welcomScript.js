7acba9e10db68d8486359a2127fe104ec4d14655
Aronex <90198844+AronZcz@users.noreply.github.com>
2022-03-09 16:23:16 +0000

    First element of site.

---
+++ FredyFnafFanPage/welcomScript.js
@@ -0,0 +1,45 @@
+var audioList = new Array(7);
+
+
+var alert_infor = document.getElementById("alert_box");// pasek alert
+var main_container = document.getElementById("main_container");
+var fredyPic = document.getElementsByClassName("pic");
+
+function isPlaying(audio)
+{
+    if (audio.duration > 0 && !audio.paused) 
+    {
+        return true;
+    } 
+    else
+    {
+        return false;
+    }
+}
+
+function init() 
+{
+    for(let i = 0; i < 7; i++)
+    {
+        audioList[i] = new Audio(fredyPic[i].innerHTML);
+
+        fredyPic[i].onclick = () => 
+        {
+            for(let j = 0; j < 7; j++)
+            {
+                if(!isPlaying(audioList[j]))
+                {
+                    audioList[i].play();
+                }
+                else
+                {
+                    audioList[j].pause();
+                    audioList[j].currentTime = 0;
+                    audioList[i].play();
+                }
+            }
+        }
+    }
+}
+
+init();
